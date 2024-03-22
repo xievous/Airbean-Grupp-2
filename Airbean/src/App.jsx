92817417d11967c856e0030de5feb1a58ad7b0b6
Oscar <oscar.la02@outlook.com>
2024-03-22 12:01:29 +0100

    routing

--- Airbean/src/App.jsx
+++ Airbean/src/App.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Landing from "./pages/Landing/Landing";
+import About from "./pages/About/About";
+import Menu from "./pages/Menu/Menu";
+import Status from "./pages/Status/Status";
 
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/menu",
+      element: <Menu />,
+    },
+    {
+      path: "/status",
+      element: <Status />,
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
