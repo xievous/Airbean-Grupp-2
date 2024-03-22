8e05b88e4da815a5e0bc615cba38d961816962cc
pearl.mari <maria.vassilikopoulou@iths.se>
2024-03-22 20:16:15 +0100

    Add the Landing page

--- Airbean/src/pages/Landing/Landing.jsx
+++ Airbean/src/pages/Landing/Landing.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom';
 import "./landing.css";
 
-const Landing = () => {
-  return <div>Landing</div>;
+const Landing = (props) => {
+  const {landing} = props;
+  const navigate = useNavigate();
+
+  function handleClick(){
+    navigate('/menu', {state: { event:landing}});
+  }
+  return (
+    <div onClick={ handleClick } className="container">
+    <div className="image-container">
+      <img src="./assets/intro-graphic-left.svg" alt="Airbean Logo" />
+      <img src="./assets/airbean-landing.svg" alt="Airbean Logo" />
+      <img src="./assets/intro-graphic-right.svg" alt="Airbean Logo" />
+    </div>
+  </div>
+  )
 };
 
 export default Landing;
+
+
+
+
+
+ 
