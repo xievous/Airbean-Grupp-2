c01eec510c8e21c17856e6622373e32a3e52b751
kemor <kamil.romanowicz95@gmail.com>
2024-03-25 17:43:49 +0100

    update

--- Airbean/src/components/Coffee-type/Coffee_type.jsx
+++ Airbean/src/components/Coffee-type/Coffee_type.jsx
@@ -1,10 +1,16 @@
 import "./coffee-type.css";
+import { useState } from "react";
 
 function Coffee_type(props) {
+
+  function handleClick() {
+    const {count, setCounter} = useState(0)
+    
+  }
   
   return (
       <article className="product-box">
-          <img src="../../assets/add.svg" alt="" className="plus" />
+          <img onClick={handleClick} src="../../assets/add.svg" alt="" className="plus" />
           <div className="title-box">
             <h2>{props.title}</h2>
             <p>{props.desc}</p>
