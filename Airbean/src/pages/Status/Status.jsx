808325856b39d0be73e33c8f039731060c54cf6a
Oscar <oscar.la02@outlook.com>
2024-03-26 11:06:50 +0100

    test

--- Airbean/src/pages/Status/Status.jsx
+++ Airbean/src/pages/Status/Status.jsx
@@ -3,6 +3,26 @@ import "./status.css";
 
 const Status = () => {
   return <div>Status</div>;
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    async function getOrder() {
+      const response = await fetch(
+        "https://airbean-9pcyw.ondigitalocean.app/order"
+      );
+      const data = await response.json();
+      setOrders(data);
+      console.log(data);
+    }
+
+    getOrder();
+  }, []);
+
+  const statusComponents = orders.map((order) => (
+    <StatusComponent eta={order.eta} key={order.orderNr} />
+  ));
+
+  return <div>{statusComponents}</div>;
 };
 
 export default Status;
