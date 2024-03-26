2975457097827b7b12748551101b5c23792e41fd
Oscar <oscar.la02@outlook.com>
2024-03-26 11:46:48 +0100

    commit

--- Airbean/src/pages/Menu/Menu.jsx
+++ Airbean/src/pages/Menu/Menu.jsx
@@ -1,61 +1,56 @@
 import React from "react";
 import "./menu.css";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { add } from "../../store/cartSlice";
 import Coffee_type from "../../components/Coffee-type/Coffee_type";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Menu = () => {
-
-  const [beans, setBeans] = useState([])
+  const [beans, setBeans] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    async function getBeans(){
-      const response = await fetch('https://airbean-9pcyw.ondigitalocean.app/api/beans/')
-      const data = await response.json()
-      console.log(data.menu)
-      setBeans(data.menu)
+    async function getBeans() {
+      const response = await fetch(
+        "https://airbean-9pcyw.ondigitalocean.app/api/beans/"
+      );
+      const data = await response.json();
+      console.log(data.menu);
+      setBeans(data.menu);
     }
 
-    getBeans()
-  }, [])
+    getBeans();
+  }, []);
 
   const addToCart = (product) => {
     dispatch(add(product));
-    console.log('product purchased')
+    console.log("product purchased");
   };
 
-  const menuComponents = beans.length > 0 ? (
-    beans.map(bean => (
-      <div key={bean.id}>
-        <Coffee_type
-          title={bean.title}
-          desc={bean.desc}
-          price={bean.price}
-        />
-        <button variant="primary" onClick={() => addToCart(bean)}>
-          Add to cart
-        </button>
-      </div>
-    ))
-  ) : (
-    <p>Loading...</p>
-  );
-  
-  
+  const menuComponents =
+    beans.length > 0 ? (
+      beans.map((bean) => (
+        <div key={bean.id}>
+          <Coffee_type title={bean.title} desc={bean.desc} price={bean.price} />
+          <button variant="primary" onClick={() => addToCart(bean)}>
+            Add to cart
+          </button>
+        </div>
+      ))
+    ) : (
+      <p>Loading...</p>
+    );
+
   return (
     <main className="menu">
       <Navbar />
-      <div className="menu-box">Menu
-        <section>
-          {menuComponents}
-        </section>
+      <div className="menu-box">
+        Menu
+        <section>{menuComponents}</section>
       </div>
     </main>
-      
-  )
+  );
 };
 
 export default Menu;
